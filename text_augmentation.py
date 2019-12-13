36d1771bb7cbbabef7fed769f20268f0996952f6
Weizhen Sheng <sheng.weizhen@gmail.com>
2019-12-12 23:57:36 -0500

    emoji predictor modifications

--- text_augmentation.py
+++ text_augmentation.py
@@ -18,8 +18,7 @@ def load_file(filename):
            vocab: list of unique tokens in all text messages
            y: list of most frequent emoji in each text message
   '''
-  RE_EMOJI = re.compile(r'\\u[0-9a-z]{4}')
-  
+  RE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
   # helper function to parse data from each line of file
   def parse_line(s):
     split_line = s.split(': ', 1)
@@ -27,43 +26,41 @@ def load_file(filename):
     text_emoji = split_line[1]
 
     # get text and most frequent emoji
-    text = RE_EMOJI.sub(r'', text_emoji)
+    text = RE_EMOJI.sub('', text_emoji).strip()
     all_emojis = RE_EMOJI.findall(text_emoji)
-    _, emoji = max([(all_emojis.count(e),e) for e in set(e)])
+    _, emoji = max([(all_emojis.count(e),e) for e in set(all_emojis)])
 
-    return text, doc, emoji
-
+    return text, emoji
+  
   texts = []
   y = []
-  document_names = []
-  vocab_set = set{}
+  vocab_set = set()
 
-  with open(filename, 'rb') as f:
+  with open(filename, 'r', encoding='utf-8') as f:
     for line in f:
-      m = RE_EMOJI.match(line)
+      m = RE_EMOJI.findall(line)
       if m:
-        text, doc, emoji = parse_line(line)
-        texts.append(text)
-        document_names.append(doc)
-        y.append(emoji)
-        for token in text.split():
-          vocab_set.add(token) 
+        text, emoji = parse_line(line)
+        if (text != ''):
+          texts.append(text)
+          y.append(emoji)
+          for token in text.split():
+            vocab_set.add(token) 
 
   vocab = list(vocab_set)
-  line_tuples = list(zip(document_names, [text.split() for text in texts]))
+  line_tuples = [text.split() for text in texts]
 
-  return line_tuples, document_names, vocab, y
+  return line_tuples, vocab, y
 
 def load_sentence(sentence):
-  vocab_set = set{}
+  vocab_set = set()
   for token in sentence.split():
     vocab_set.add(token)
   vocab = list(vocab_set)
+
+  line_tuples = [sentence.split()]
   
-  line_tuples = [tuple(('test_doc', [sentence.split()]))]
-  document_names = ['test_doc']
-  
-  return line_tuples, document_names, vocab
+  return line_tuples, vocab
 
 def precision(y_pred, y_true):
   numerator = sum([ 1 if y_pred[i] == 1 and y_true[i] == 1 else 0 for i in range(len(y_pred)) ])
@@ -76,11 +73,11 @@ def recall(y_pred, y_true):
   return float(numerator) / float(denominator)
 
 def fscore(y_pred, y_true):
-  precision = get_precision(y_pred, y_true)
-  recall = get_recall(y_pred, y_true)
-  return float(2 * precision * recall) / float(precision + recall)
+  prec = precision(y_pred, y_true)
+  rec = recall(y_pred, y_true)
+  return float(2 * prec * rec) / float(prec + rec)
 
-def create_td_matrix(line_tuples, document_names, vocab):
+def create_td_matrix(line_tuples, vocab):
   '''
   Inputs:
     line_tuples: list of tuples containing the name of the document and a tokenized line from that document
@@ -91,16 +88,14 @@ def create_td_matrix(line_tuples, document_names, vocab):
     td_matrix: mxn numpy array where A_ij contains the frequency with which word i occurs in document j
   '''
   vocab_to_id = dict(zip(vocab, range(0, len(vocab))))
-  docname_to_id = dict(zip(document_names, range(0, len(document_names))))
+  # docname_to_id = dict(zip(document_names, range(0, len(document_names))))
 
-  td_matrix = np.empty(shape=(len(vocab), len(document_names)))
+  td_matrix = np.empty(shape=(len(vocab), len(line_tuples)))
 
-  for line_tuple in line_tuples:
-    document_name, tokenized_line = line_tuple
-    for word in tokenized_line:
-      doc_id = docname_to_id[document_name]
+  for i,line in enumerate(line_tuples):
+    for word in line:
       vocab_id = vocab_to_id[word]
-      td_matrix[vocab_id][doc_id] += 1
+      td_matrix[vocab_id][i] += 1
   return td_matrix
 
 def create_tf_idf_matrix(term_document_matrix):
@@ -120,51 +115,121 @@ def create_tf_idf_matrix(term_document_matrix):
     for doc in range(0, N):
       count = term_document_matrix[word][doc]
       tf = float(1 + math.log10(count)) if count > 0 else 0
-      idf = float(math.log10(N / word_count[word]))
+      idf = float(math.log10(N / word_count[word])) if word_count[word] > 0 else 0
       result[word][doc] = tf * idf
 
   return result
 
-def naive_bayes(train_x, train_y, dev_x, dev_y, test_x):
-  train_x_normalized = (train_x - train_x.mean(axis=0)) / train_x.std(axis=0)
-  dev_x_normalized = (dev_x - dev_x.mean(axis=0)) / dev_x.std(axis=0)
-  test_x_normalized = (test_x - test_x.mean(axis=0)) / test_x.std(axis=0)
+def match_features(features_1, features_2, vocab_1, vocab_2):
+  result = np.zeros(features_1.shape)
+  print(result.shape)
+  for i in range(len(vocab_1)):
+    if vocab_1[i] in vocab_2:
+      index = np.where(vocab_2 == vocab_1[i])
+      print(index)
+      result[i] = features_2[index[0]]
+  return result
+
+def naive_bayes(train_x, train_y):
+  train_x_std = np.array(train_x.std(axis=0))
+  # dev_x_std = np.array(dev_x.std(axis=0))
+  # test_x_std = np.array(test_x.std(axis=0))
+  train_x_normalized = np.array(train_x - train_x.mean(axis=0))
+  # dev_x_normalized = np.array(dev_x - dev_x.mean(axis=0))
+  # test_x_normalized = np.array(test_x - test_x.mean(axis=0))
+  train_x_normalized = np.divide(train_x_normalized, train_x_std, out=np.zeros_like(train_x_normalized), where=train_x_std!=0)
+  # dev_x_normalized = np.divide(dev_x_normalized, dev_x_std, out=np.zeros_like(dev_x_normalized), where=dev_x_std!=0)
+  # test_x_normalized = np.divide(test_x_normalized, test_x_std, out=np.zeros_like(test_x_normalized), where=test_x_std!=0)
+  train_x_normalized = np.transpose(train_x_normalized)
+  # dev_x_normalized = np.transpose(dev_x_normalized)
+  # test_x_normalized = np.transpose(test_x_normalized)
+
+  train_y_np = np.array(train_y)
+  # dev_y_np = np.array(dev_y)
 
   clf = GaussianNB()
-  clf.fit(train_x_normalized, train_y)
+  clf.fit(train_x_normalized, train_y_np)
 
-  ty_pred = clf.predict(train_x_normalized)
-  tprecision = precision(ty_pred, train_y)
-  trecall = recall(ty_pred, train_y)
-  tfscore = fscore(ty_pred, train_y)
+  # print(train_x_normalized.shape, train_y_np.shape)
+  # ty_pred = clf.predict(train_x_normalized)
+  # # tprecision = precision(ty_pred, train_y_np)
+  # # trecall = recall(ty_pred, train_y_np)
+  # # tfscore = fscore(ty_pred, train_y_np)
 
-  dy_pred = clf.predict(dev_x_normalized)
-  dprecision = precision(dy_pred, dev_y)
-  drecall = recall(dy_pred, dev_y)
-  dfscore = fscore(dy_pred, dev_y)
+  # print(dev_x_normalized.shape, dev_y_np.shape)
+  # dy_pred = clf.predict(dev_x_normalized)
+  # # dprecision = precision(dy_pred, dev_y_np)
+  # # drecall = recall(dy_pred, dev_y_np)
+  # # dfscore = fscore(dy_pred, dev_y_np)
 
-  y_pred = clf.predict(test_x_normalized)
+  # print(test_x_normalized.shape)
+  # y_pred = clf.predict(test_x_normalized)
 
-  train_performance = (tprecision, trecall, tfscore)
-  dev_performance = (dprecision, drecall, dfscore)
-  return train_performance, dev_performance, y_pred
+  # train_performance = (tprecision, trecall, tfscore)
+  # dev_performance = (dprecision, drecall, dfscore)
+  return clf
 
 if __name__ == '__main__':
-    sentence_to_augment = sys.argv[1]
+    texts = ["i love you", "oh i'm sorry i'm so lit i try to be funny", "I like the look of that dress"]
 
-    line_tuples_train, document_names_train, vocab_train, train_y = load_file('data/train.txt')
-    line_tuples_dev, document_names_dev, vocab_dev, dev_y = load_file('data/dev.txt')
-    line_tuples_test, document_names_test, vocab_test = load_sentence(sentence_to_augment)
+    line_tuples_train, vocab_train, train_y = load_file('emoji_file_train.txt')
+    line_tuples_dev, vocab_dev, dev_y = load_file('emoji_file_dev.txt')
 
-    td_train = create_td_matrix(line_tuples_train, document_names_train, vocab_train)
-    td_dev = create_td_matrix(line_tuples_dev, document_names_dev, vocab_dev)
-    td_test = create_td_matrix(line_tuples_test, document_names_test, vocab_test)
+    emoji_to_int = dict()
+    int_to_emoji = dict()
+    emoji_to_freq = dict()
+
+    list_of_emojis = []
+    new_y = []
+    counter = 0
+    for emoji in train_y:
+      if (emoji in emoji_to_int):
+        new_y.append(emoji_to_int[emoji])
+        emoji_to_freq[emoji] += 1
+      else:
+        new_y.append(counter)
+        emoji_to_int[emoji] = counter
+        int_to_emoji[counter] = emoji 
+        emoji_to_freq[emoji] = 1
+        counter += 1
+    train_y = new_y
+
+    # sorted_freq = {k: v for k, v in sorted(emoji_to_freq.items(), key=lambda item: item[1], reverse=True)}
+    # for e,f in sorted_freq.items():
+    #   print(e, ': ', str(f))
+
+    new_y = []
+    for emoji in dev_y:
+      if emoji in emoji_to_int:
+        new_y.append(emoji_to_int[emoji])
+    dev_y = new_y
+
+    vocab = set(vocab_train + vocab_dev)
+
+    td_train = create_td_matrix(line_tuples_train, vocab)
+    # td_dev = create_td_matrix(line_tuples_dev, vocab)
 
     tf_idf_train = create_tf_idf_matrix(td_train)
-    tf_idf_dev = create_tf_idf_matrix(td_dev)
-    tf_idf_test = create_tf_idf_matrix(td_test)
+    # tf_idf_dev = create_tf_idf_matrix(td_dev)
 
-    train_performance, dev_performance, y_pred = naive_bayes(tf_idf_train, train_y, tf_idf_dev, dev_y, tf_idf_test)
-    print("Training performance: ", train_performance)
-    print("Development performance: ", dev_performance)
-    print("Sentence to augment: ", sentence_to_augment, " | Suggested emoji: ", y_pred)
+    print(tf_idf_train.shape)
+    # print(tf_idf_dev.shape)
+
+    # matched_train = match_features(tf_idf_train, tf_idf_dev, vocab_train, vocab_dev)
+    # matched_test = match_features(tf_idf_train, tf_idf_test, vocab_train, vocab_test)
+    clf = naive_bayes(tf_idf_train, train_y)
+
+    for t in texts:
+      print(t)
+      line_tuples_test, vocab_test = load_sentence(t)
+      td_test = create_td_matrix(line_tuples_test, vocab)
+      test_x = create_tf_idf_matrix(td_test)
+
+      test_x_std = np.array(test_x.std(axis=0))
+      test_x_normalized = np.array(test_x - test_x.mean(axis=0))
+      test_x_normalized = np.divide(test_x_normalized, test_x_std, out=np.zeros_like(test_x_normalized), where=test_x_std!=0)
+      test_x_normalized = np.transpose(test_x_normalized)
+
+      y_pred = clf.predict(test_x_normalized)
+
+      print("Sentence to augment: ", t, " | Suggested emoji: ", int_to_emoji[y_pred[0]])
